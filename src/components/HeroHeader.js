1fdf0966bb160af81f52afff837b7e227a303539
Christopher Patty <christopher.jon.patty@gmail.com>
2018-07-13 19:36:04 -0600

    Added image url to header toml

--- src/components/HeroHeader.js
+++ src/components/HeroHeader.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from 'react-emotion'
+
+const Wrapper = styled('div')({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'row'
+})
+
+const ProfileImage = styled('img')({
+  
+})
 
 export default class HeroHeader extends React.Component{
   render(){
     const { header } = this.props;
+    const { profileUrl } = header;
     console.log(header);
     return(
-      <div>Hero Header</div>
+      <Wrapper>
+        <ProfileImage src={profileUrl} />
+      </Wrapper>
     )
   }
 }
